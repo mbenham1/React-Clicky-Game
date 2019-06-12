1f4ed791d3a704a7d735fc0de6de75c9fbee4e26
Benham <Benham@Brets-MacBook-Air.local>
2019-06-12 18:39:25 -0400

    added media query- nav

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,7 +8,7 @@ function NavBar(props) {
 
         <li><strong>Clicky Game</strong></li>
         <li className={props.lose !== null ? 
-  (props.lose ? "message text-danger" : "message" ) : "message"} ><span>{props.message}</span></li>
+        (props.lose ? "message text-danger" : "message" ) : "message"} ><span>{props.message}</span></li>
         {/* <li className={props.lose ? "message text-danger" : "message text-success"}><span>{props.message}</span></li> */}
         <li>Score: <span>{props.counter}</span> | Top Score: <span>{props.high_score}</span></li>
 
