f00c235af870e202d8722fce2720e7179cc43ff6
Benham <Benham@Brets-MacBook-Air.local>
2019-06-13 15:01:00 -0400

    Updates

---
+++ precache-manifest.b6bb5f582707bdfced5f8e2bb22727d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce949edfc32f983070cffeabc2a409cc",
+    "url": "/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "ce5422f7c9ddfdb3c69c",
+    "url": "/React-Clicky-Game/static/css/main.028dd5f2.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/React-Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ce5422f7c9ddfdb3c69c",
+    "url": "/React-Clicky-Game/static/js/main.b8f85dee.chunk.js"
+  },
+  {
+    "revision": "339ef2b4a9d9b3bbcf8d",
+    "url": "/React-Clicky-Game/static/js/runtime~main.25e3a6ff.js"
+  }
+]);
